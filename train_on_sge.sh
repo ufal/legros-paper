f891ca8eefdfba3734a73797ff227c18b4b1b4f4
Jindrich Libovicky <libovicky@ufal.mff.cuni.cz>
2022-05-27 18:11:59 +0200

    debug training pipeline

--- train_on_sge.sh
+++ train_on_sge.sh
@@ -9,12 +9,12 @@ TARGET_VOCAB_SIZE=$4
 
 TMP=$(mktemp experiments/XXXXX)
 
-VOCAB_CYCLE_STEPS=10 # Number of times that we sample bigrams
-VOCAB=${TMP}.vocab
+VOCAB_CYCLE_STEPS=5 # Number of times that we sample bigrams
+VOCAB=${TMP}.vocab.0
 
 cp ${SEED_VOCAB} ${VOCAB}
 
-# TODO wrap this in one large for loop for decreasing vocab size
+CYCLE=0
 while true; do
     VOCAB_SIZE=$(wc -l < $VOCAB)
 
@@ -23,19 +23,19 @@ while true; do
 
     MAX_FILES=$(ls ${TMP}.split.* | wc -l)
 
-    qsub -t 1-${MAX_FILES} -N sample_bigrams -pe smp 1 -e ${TMP}.bigrams.0.log -o ${TMP}.bigrams.0 -sync y \
+    qsub -t 1-${MAX_FILES} -N sample_bigrams -pe smp 1 -e ${TMP}.bigrams.${CYCLE}.0.log -o ${TMP}.bigrams.${CYCLE}.0 -sync y \
         qsub_array_sample_bigrams.sh $VOCAB uniform ${TMP}.split
 
-    python -m neuralpiece.train_estimator $VOCAB ${TMP}.bigrams.0 ${TMP}.model.0 2>&1 | tee ${TMP}.model.0.log
-    python -m neuralpiece.tokenize $VOCAB ${TMP}.model.0.numpy $VALID | tee ${TMP}.valid.0
+    python -m neuralpiece.train_estimator $VOCAB ${TMP}.bigrams.${CYCLE}.0 ${TMP}.model.${CYCLE}.0 2>&1 | tee ${TMP}.model.${CYCLE}.0.log
+    python -m neuralpiece.tokenize $VOCAB ${TMP}.model.${CYCLE}.0.numpy $VALID | tee ${TMP}.valid.${CYCLE}.0
 
     for I in `seq 1 ${VOCAB_CYCLE_STEPS}`; do
         rm ${TMP}.split.*
         shuf $CORPUS | head -n 50000 | split -l 80 --numeric-suffixes=1 -a 5 - ${TMP}.split.
         MAX_FILES=$(ls ${TMP}.split.* | wc -l)
 
-        qsub -t 1-${MAX_FILES} -N sample_bigrams -e ${TMP}.bigrams.${I}.log -o ${TMP}.bigrams.${I} -sync y \
-            qsub_array_sample_bigrams.sh $VOCAB ${TMP}.model.$((I - 1)).numpy ${TMP}.split
+        qsub -t 1-${MAX_FILES} -N sample_bigrams -e ${TMP}.bigrams.${CYCLE}.${I}.log -o ${TMP}.bigrams.${CYCLE}.${I} -sync y \
+            qsub_array_sample_bigrams.sh $VOCAB ${TMP}.model.${CYCLE}.$((I - 1)).numpy ${TMP}.split
 
         python -m neuralpiece.train_estimator \
             $VOCAB ${TMP}.bigrams.${I} ${TMP}.model.${I} \
@@ -52,6 +52,8 @@ while true; do
 
     NEW_VOCAB_SIZE=$(echo "print(max(${TARGET_VOCAB_SIZE}, int(0.95 * ${VOCAB_SIZE})))")
     python -m neuralpiece.reduce_vocab ${VOCAB} ${TMP}.model.${VOCAB_CYCLE_STEPS} > ${TMP}.new_vocab
-    mv ${TMP}.new_vocab ${TMP}.vocab
-done
 
+    CYCLE=$(( CYCLE + 1 ))
+    VOCAB=${TMP}.vocab.${CYCLE}
+    mv ${TMP}.new_vocab ${VOCAB}
+done
